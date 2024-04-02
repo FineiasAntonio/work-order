e07165a130b0b758c4c3391e39a57b52513175da
Finéias Antônio <fineias1187@gmail.com>
2024-04-01 21:25:57 -0300

    Revert ".env"
    This reverts commit fec874ade1f0fdea432df5c43dd0f0b3e3a03871.

--- frontend/src/Service/Firebase/Firebase.tsx
+++ frontend/src/Service/Firebase/Firebase.tsx
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyAJ-u02n0R7Rz_0sK45zNg3LbLM4JE65Jw",
+  authDomain: "eletroficina-galvao.firebaseapp.com",
+  projectId: "eletroficina-galvao",
+  storageBucket: "eletroficina-galvao.appspot.com",
+  messagingSenderId: "601631816395",
+  appId: "1:601631816395:web:1ef3eaf8dddfc88790394e",
+  measurementId: "G-HCKGHX7XJP"
 };
 
 const app = initializeApp(firebaseConfig);
--- frontend/src/Service/api/api.tsx
+++ frontend/src/Service/api/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-    baseURL: process.env.API_URL,
+    baseURL: "http://localhost:8080",
     timeout: 3000,
     timeoutErrorMessage: "Connection timeout",
     validateStatus: () => true
