a81f806c6bae5eb35854f9f10e8d4433cee439fa
Finéias Antônio <138174957+FineiasAntonio@users.noreply.github.com>
2024-06-02 14:28:08 -0300

    :wrench: chore: nginx configurable in host

--- API/src/main/java/com/workordercontrol/api/WorkOrderApplication.java
+++ API/src/main/java/com/workordercontrol/api/WorkOrderApplication.java
@@ -1,13 +1,18 @@
 package com.workordercontrol.api;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@OpenAPIDefinition
+@OpenAPIDefinition(
+		servers = {
+				@Server(url = "http://localhost", description = "Default Server URL")
+		}
+)
 public class WorkOrderApplication {
 
 	public static void main(String[] args) {
