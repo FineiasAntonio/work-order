f733f715b6b167519c7572395dc35adeaed5ecf1
Finéias Antônio <138174957+FineiasAntonio@users.noreply.github.com>
2024-06-16 11:49:46 -0300

    :sparkles: feat: login page

--- UI/src/app/app.routes.ts
+++ UI/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginFormComponent } from './modules/authentication/login-form-page/login-form/login-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginFormComponent,
+  },
+];
